39da7de8a0cac9ba2eacf3142269c221d5b04f74
Noah Tajwar <noah.tajwar.oss@gmail.com>
2020-03-11 22:01:46 -0700

    Implemented Kinematics math

--- firmware/lib/Gantry/Kinematics.cpp
+++ firmware/lib/Gantry/Kinematics.cpp
@@ -1,4 +1,28 @@
 #include "Kinematics.h"
-#include "Timer.h"
 
-// math will go here
+#define MOTOR_MICROSTEP        4
+#define MOTOR_STEPS_PER_REV    (200 * MOTOR_MICROSTEP)
+#define ENCODER_COUNTS_PER_REV 300
+
+static uint32_t calc_counts_accel(uint32_t accel, uint32_t v_min, uint32_t v_max)
+{
+    // Calculate number of motor steps during which to accelerate
+    uint32_t steps_accel = ((v_max * v_max) - (v_min * v_min)) / (2 * accel);
+
+    // Convert to number of encoder counts
+    return (steps_accel * ENCODER_COUNTS_PER_REV / MOTOR_STEPS_PER_REV);
+}
+
+bool generate_vel_profile(uint32_t accel, uint32_t v_min, uint32_t v_max, uint32_t counts_total, VelProfile *profile_out)
+{
+    if (accel == 0) return false;
+
+    uint32_t counts_accel = calc_counts_accel(accel, v_min, v_max);
+    uint32_t counts_hold = (counts_total - (2 * counts_accel));
+
+    profile_out->counts_accel;
+    profile_out->counts_hold;
+    profile_out->counts_accel;
+
+    return true;
+}
--- firmware/lib/Gantry/Kinematics.h
+++ firmware/lib/Gantry/Kinematics.h
@@ -1,1 +1,14 @@
-// math will go here
+#ifndef KINEMATICS_H
+#define KINEMATICS_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint32_t counts_accel;
+    uint32_t counts_hold;
+    uint32_t counts_decel;
+} VelProfile;
+
+bool generate_vel_profile(uint32_t accel, uint32_t v_min, uint32_t v_max, uint32_t counts_total, VelProfile *profile_out);
+
+#endif // KINEMATICS_H

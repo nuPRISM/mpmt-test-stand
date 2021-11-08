2b9ca08d823f62f04912922cc3b8c60911b5abbf
Thomas Lindner <lindner@triumf.ca>
2021-11-08 10:55:31 -0800

    Working gantry move test.

--- Scan/gantry_repeat_move.sh
+++ Scan/gantry_repeat_move.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
 
+echo "Starting repetition of 10 loops with gantry"
 
-for i in {1..10}
+
+for i in {1..2}
 do
 
     # Move X minus
     echo 'starting X minus'
-    odbedit -c "set /Equipment/Motors00/Settings/Destination[1] -20000"
+    odbedit -c "set /Equipment/Motors00/Settings/Destination[1] -40000"
     odbedit -c "set /Equipment/Motors00/Settings/Move[1] y"
-    sleep 10
-    odbedit -c "set /Equipment/Motors00/Settings/Move[1] n"
+    sleep 50
     
     # Move Y minus
     echo 'starting Y minus'
-    odbedit -c "set /Equipment/Motors00/Settings/Destination[2] -20000"
+    odbedit -c "set /Equipment/Motors00/Settings/Destination[2] -40000"
     odbedit -c "set /Equipment/Motors00/Settings/Move[2] y"
-    sleep 10
-    odbedit -c "set /Equipment/Motors00/Settings/Move[2] n"
+    sleep 50
     
     # Move X plus
     echo 'start X plus'
-    odbedit -c "set /Equipment/Motors00/Settings/Destination[1] 20000"
+    odbedit -c "set /Equipment/Motors00/Settings/Destination[1] 40000"
     odbedit -c "set /Equipment/Motors00/Settings/Move[1] y"
-    sleep 10
-    odbedit -c "set /Equipment/Motors00/Settings/Move[1] n"
+    sleep 50
+
     
     # Move Y plus
     echo 'start Y plus'
-    odbedit -c "set /Equipment/Motors00/Settings/Destination[2] 20000"
+    odbedit -c "set /Equipment/Motors00/Settings/Destination[2] 40000"
     odbedit -c "set /Equipment/Motors00/Settings/Move[2] y"
-    sleep 10
-    odbedit -c "set /Equipment/Motors00/Settings/Move[2] n"
+    sleep 50
     
 done
 
